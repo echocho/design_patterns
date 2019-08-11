75184b6234b9f364d9bc3c8fc322869c303530b6
echocho <echo333cheng@gmail.com>
2019-08-11 23:21:26 +0800

    split files

--- builder_pattern/vacation_planner.py
+++ builder_pattern/vacation_planner.py
@@ -71,26 +71,3 @@ class OneDayPlan(object):
         self.restaurant = {"name": None, "date": self.activity_date}
         self.special_event = {"name": None, "date": self.activity_date}
         self.ticket = {"name": None, "date": self.activity_date}
-
-
-def construct_planner():
-    builder = AbstractBuilder()
-    builder.build_day('2019-09-11')
-    builder.add_hotel('Hilton Beijing')
-    builder.add_restaurant('UTown')
-    builder.add_special_event('Visiting a friend')
-    builder.add_ticket('Forbidden city')
-
-    builder.build_day('2019-09-12')
-    builder.add_hotel('Hilton Beijing')
-    builder.add_restaurant('UTown')
-
-    # FIXME: should display multiple tickets if there are so
-    builder.build_day('2019-09-13')
-    builder.add_ticket('Forbidden city')
-    builder.add_ticket('798 Picasso')
-
-    print(builder.get_plans())
-    builder.print_vacation_plans()
-
-construct_planner()
